e4eb8c56497093ac54a68aed7294becae47baef5
Tomasz Dopierala <dopiet@wardx.net>
2017-07-11 13:16:10 +0200

    Adding vm omicron-web03 with Nginx server

---
+++ omicron-web03/provisioners/bootstrap.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echo -e "\n=> Updating packages list\n"
+apt-get update
+
+echo -e "\n=> Install base packages\n"
+apt-get -y install vim curl build-essential python-software-properties git zip unzip >> /vagrant/log/vm_build.log 2>&1
+
+#echo -e "\n=> Installing Java JRE\n"
+#apt-get -y install default-jre >> /vagrant/log/vm_install.log 2>&1
+
+#apt-get -y install libstdc++5 >> /vagrant/log/vm_install.log 2>&1
+
+#echo -e "\n=> Installing ColdFusion 11 Server\n"
+#cp /mnt/storage/ColdFusion_11_WWEJ_linux64.bin /home/vagrant/ >> /vagrant/log/vm_build.log 2>&1
+#cd /home/vagrant/ >> /vagrant/log/vm_build.log 2>&1
+#./ColdFusion_11_WWEJ_linux64.bin [-f /vagrant/configs/cf11_silent.properties] >> /vagrant/log/vm_build.log 2>&1
+
+#echo -e "\n=> Setting document root to public directory\n"
+#ln -s /mnt/repo/cf.pl.malpkaexpress.intranet/ /var/www/html/intranet >> /vagrant/log/vm_build.log 2>&1
+
+#if ! [ -L /var/www ]; then
+#  rm -rf /var/www
+#  ln -fs /vagrant /var/www
+#fi
+
+#echo -e "\n=> Restarting Apache\n"
+#service apache2 restart >> /vagrant/log/vm_build.log 2>&1
+
+#filename="vm_install.log";
+#touch /vagrant/log/${filename}
+
+#echo -e "\n--- installing defults\n"
+#cd /vagrant-base/provisioners/ >> /vagrant/log/vm_install.log 2>&1
+#bash ./install-defaults.sh >> /vagrant/log/vm_install.log 2>&1
+
+echo -e "\n=> Aditional settings\n"
+cat /vagrant/configs/etc/motd > /etc/motd
+#cat /vagrant/configs/etc/php7.1.ini > /etc/php/7.1/apache2/php.ini
---
+++ omicron-web03/provisioners/vhosts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo -e "\n=> Setting vhosts\n"
+cat /vagrant/configs/vhosts/000-default.conf > /etc/apache2/sites-available/000-default.conf
+cat /vagrant/configs/vhosts/default-ssl.conf > /etc/apache2/sites-available/default-ssl.conf
+cat /vagrant/configs/vhosts/local.wardx.net.conf > /etc/apache2/sites-available/local.wardx.net.conf
+cat /vagrant/configs/vhosts/local.dopierala.net.pl.conf > /etc/apache2/sites-available/local.dopierala.net.pl.conf
+cat /vagrant/configs/vhosts/local.skynet.omicron.net.pl.conf > /etc/apache2/sites-available/local.skynet.omicron.net.pl.conf
+cat /vagrant/configs/vhosts/local.mercudo.wardx.net.conf > /etc/apache2/sites-available/local.mercudo.wardx.net.conf
+
+echo -e "\n=> Enabling vhosts\n"
+a2ensite local.wardx.net.conf >> /vagrant/log/vhosts.log 2>&1
+a2ensite local.dopierala.net.pl.conf >> /vagrant/log/vhosts.log 2>&1
+a2ensite local.skynet.omicron.net.pl.conf >> /vagrant/log/vhosts.log 2>&1
+a2ensite local.mercudo.wardx.net.conf >> /vagrant/log/vhosts.log 2>&1
+
+echo -e "\n=> Reloading Apache\n"
+service apache2 reload >> /vagrant/log/vhosts.log 2>&1
