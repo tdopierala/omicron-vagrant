222d0bde99f71b585c1e8fb8c3016bf7c4003f0f
Tomasz Dopierala <bitbucket@dopierala.net.pl>
2019-01-30 17:40:06 +0100

    Update for omicron-web03

--- omicron-web03/provisioners/bootstrap.sh
+++ omicron-web03/provisioners/bootstrap.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-echo -e "\n=> Updating packages list\n"
-apt-get update
+echo -e "\n"
 
-echo -e "\n=> Install base packages\n"
-apt-get -y install vim curl build-essential python-software-properties git zip unzip >> /vagrant/log/vm_build.log 2>&1
+echo -e "\n=> Updating packages list...\n"
+apt-get update >> /vagrant/log/vm_build.log 2>&1
+apt-get upgrade >> /vagrant/log/vm_build.log 2>&1
+
+echo -e "\n=> Install base packages...\n"
+apt-get -y install vim net-tools curl build-essential software-properties-common git zip unzip >> /vagrant/log/vm_build.log 2>&1
+
+echo -e "\n=> Installing Apache Server...\n"
+apt-get -y install apache2 >> /vagrant/log/vm_build.log 2>&1
+
+echo -e "\n=> Installing Lucee Server...\n"
+wget https://cdn.lucee.org/lucee-5.2.8.050-pl0-linux-x64-installer.run
+chmod +x lucee-5.2.8.050-pl0-linux-x64-installer.run
+#./lucee-5.2.8.050-pl0-linux-x64-installer.run
 
 #echo -e "\n=> Installing Java JRE\n"
 #apt-get -y install default-jre >> /vagrant/log/vm_install.log 2>&1
@@ -34,6 +45,6 @@ apt-get -y install vim curl build-essential python-software-properties git zip u
 #cd /vagrant-base/provisioners/ >> /vagrant/log/vm_install.log 2>&1
 #bash ./install-defaults.sh >> /vagrant/log/vm_install.log 2>&1
 
-echo -e "\n=> Aditional settings\n"
-cat /vagrant/configs/etc/motd > /etc/motd
+#echo -e "\n=> Aditional settings\n"
+#cat /vagrant/configs/etc/motd > /etc/motd
 #cat /vagrant/configs/etc/php7.1.ini > /etc/php/7.1/apache2/php.ini
