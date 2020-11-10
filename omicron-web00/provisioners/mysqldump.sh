d677b325701c185b16795aa0b20282f7cd6fb074
tdopierala <tomasz@dopierala.net.pl>
2020-11-10 14:00:27 +0100

    Latest changes

--- omicron-web00/provisioners/mysqldump.sh
+++ omicron-web00/provisioners/mysqldump.sh
@@ -4,7 +4,7 @@ if [[ -f /vagrant-dir/backup/full-backup-latest.sql ]]; then
 	mv /vagrant-dir/backup/full-backup-latest.sql /vagrant-dir/backup/full-backup-$(date +\%F)-$(echo $RANDOM | md5sum | cut -c -32).sql
 fi
 
-sudo mysqldump -u root -p --single-transaction --routines --lock-tables=false --all-databases > /vagrant-dir/backup/full-backup-latest.sql
+sudo mysqldump --single-transaction --routines --lock-tables=false --all-databases > /vagrant-dir/backup/full-backup-latest.sql
 
 IFS=' ' read -ra file <<< $(ls -lh /vagrant-dir/backup/full-backup-latest.sql)
 
